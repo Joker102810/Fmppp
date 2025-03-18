6bb61d6b6322f58e72281bf7291750ce11637043
Joker102810 <jaydenabridges9@gmail.com>
2025-03-18 10:43:50 +0000

    anxiety meter

--- Assets/Scripts/Anxiety.cs
+++ Assets/Scripts/Anxiety.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class Anxiety : MonoBehaviour
 {
-    public int mentality;
-    public int maxMental = 10;
+    public float mentality;
+    public float maxMental = 1000;
     public Slider Slider;
 
     // Start is called before the first frame update
@@ -15,7 +15,11 @@ public class Anxiety : MonoBehaviour
         mentality = maxMental;
         Slider.maxValue = maxMental;
         Slider.value = mentality;
+
+        // Call LowerMentality every second
+        InvokeRepeating("LowerMentality", 1f, 1f);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -23,19 +27,21 @@ public class Anxiety : MonoBehaviour
         
     }
 
-    public void LowerMentality(int amount)
+    public void LowerMentality()
     {
-        mentality -= amount;
-        Slider.value = mentality;
+        mentality -= 0.1f;
+        Slider.value = mentality; 
 
         if(mentality <= 0)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
-
     }
 
-     
+    public void mentalDecline()
+    { 
+        mentality = 0; 
+    }
 
 
 }
