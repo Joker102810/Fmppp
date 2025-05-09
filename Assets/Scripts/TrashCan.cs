b4e89b051b8c53fc8320401d2e14f184a50ba1b1
Joker102810 <jaydenabridges9@gmail.com>
2025-05-09 11:50:05 +0100

    dialogue instructions

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -33,7 +33,7 @@ public class TrashCan : MonoBehaviour
 
                     if (Input.GetKeyDown(KeyCode.E))
                     {
-                        SceneManager.LoadScene("Main Scene2");
+                        SceneManager.LoadScene(4);
                         Debug.Log("Minigame1");
 
                         GameObject trashObject = GameObject.FindWithTag("Trash");
